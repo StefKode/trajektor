3a9c358783114b58d5a0be010565084d8c477fd2
Stefan <S-Koch@gmx.net>
2015-06-28 15:52:48 +0200

    reduce pipline to show 2d trajectory only, limit space system to earth and prob as preparation for thruster class

--- testcases/test_basic_obj.cpp
+++ testcases/test_basic_obj.cpp
@@ -8,18 +8,19 @@ int main()
 //                         s      m      h    d
 	//double lim_sec  = (60.0 * 60.0 * 24 * 690);
 	double lim_sec  = (60.0 * 60.0 * 24 * 300);
-	double step_sec = 300;
+	double step_sec = 30;
 	double dur_sec  = 0.0;
 	double capture_sec      = 0.0;
 	double capture_step_sec = lim_sec / 2000.0;
 
-	//_________________________name_________________pos(m)_____________move(m/s)_________m(kg)_______r(m)_____
-	sys.add(new SpaceObject(  "Sun",         0,       0, 0,        0,       0, 0,    1.985e30,  696342e3));
+	//_________________________name_________________pos(m)_______________________________move(m/s)_________m(kg)_______r(m)__
+//	sys.add(new SpaceObject(  "Sun",         0,       0, 0,                             0,       0, 0,    1.985e30,  696342e3));
+//	sys.add(new SpaceObject("Earth",         0,  149.6e9,0,                       29.78e3,       0, 0,    5.972e24,   6.371e6));
+//	sys.add(new SpaceObject("probe",         0, (149.6e9+6.371e6+200e3), 0, 29.78e3+8.8e3,       0, 0,           1,         1));
+//	sys.add(new SpaceObject( "Mars",  -227.9e9,       0, 0,                             0, 24.07e3, 0,  0.64174e24,  3.3962e6));
 
-	sys.add(new SpaceObject("Earth",         0,  149.6e9,                0,     29.78e3,        0, 0,    5.972e24,   6.371e6));
-	sys.add(new SpaceObject("probe",         0, (149.6e9+6.371e6+200e3) ,0,    35.78e3, 0, 0,           1,         1));
-
-	sys.add(new SpaceObject( "Mars",  -227.9e9,       0, 0,        0, 24.07e3, 0,  0.64174e24,  3.3962e6));
+	sys.add(new SpaceObject("Earth",         0,        0,0,                             0,       0, 0,    5.972e24,   6.371e6));
+	sys.add(new SpaceObject("probe",         0, (        6.371e6+200e3), 0,         7.8e3,       0, 0,           1,         1));
 
 	sys.listObjects();
 
